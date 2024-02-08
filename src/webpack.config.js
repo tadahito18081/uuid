3a219d5ae39bebba28dd10ff189458a3695faf6a
tadahito18081 <118245868+tadahito18081@users.noreply.github.com>
2024-02-08 01:06:53 +0000

    temp

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
   // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
   mode: 'development',
   // エントリーポイントの設定
-  entry: './index.js',
+  entry: './v4.js',
   // 出力の設定
   output: {
     // 出力するファイル名
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'public/js'),
+    path: path.join(__dirname, 'public/js/v4'),
   },
   // webpack.config.jsの設定例
   resolve: {
